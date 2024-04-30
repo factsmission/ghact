b004135dab10a4d8d57e5928ac41b65c4f276f09
nleanba <25827850+nleanba@users.noreply.github.com>
2024-04-30 13:51:57 +0200

    removed unneccesary dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -9,15 +9,5 @@ export {
   serveFile,
 } from "https://deno.land/std@0.202.0/http/file_server.ts";
 
-export { existsSync, walk } from "https://deno.land/std@0.202.0/fs/mod.ts";
+export { walk } from "https://deno.land/std@0.202.0/fs/mod.ts";
 export * as path from "https://deno.land/std@0.209.0/path/mod.ts";
-
-export { parseArgs } from "https://deno.land/std@0.215.0/cli/parse_args.ts";
-
-export { DOMParser } from "https://esm.sh/linkedom@0.16.8/cached";
-
-export { iso6393To1 } from "https://esm.sh/iso-639-3@3.0.1";
-
-// broken somehow??
-// export { Element } from "https://esm.sh/v135/linkedom@0.16.8/types/interface/element.d.ts";
-// export type Element = globalThis.Element

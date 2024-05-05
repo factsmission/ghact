24377c2cd373cde91bb96ba1607d56d669849643
Reto Gmür <reto@factsmission.com>
2024-05-05 16:58:30 +0000

    typo

--- example/config.ts
+++ example/config.ts
@@ -3,7 +3,7 @@ import { type Config } from "../mod.ts";
 export const config: Config = {
   title: "GhAct Example Service",
   description:
-    "GhAct is a framework to execue a script on current and future files in a Github repository.",
+    "GhAct is a framework to execute a script on current and future files in a Github repository.",
   email: "demo@example.org",
   sourceBranch: "master",
   sourceRepository: "plazi/treatments-xml",

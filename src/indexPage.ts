6a4a9cc00518aa59c4596638bc2215454b280294
nleanba <25827850+nleanba@users.noreply.github.com>
2024-07-11 15:36:49 +0200

    only load first 200 jobs initially

--- src/indexPage.ts
+++ src/indexPage.ts
@@ -5,10 +5,11 @@ export function indexPage(
   description: string,
   jobsDB: JobsDataBase,
 ) {
-  const jobsTable = jobsDB.allJobs().map((job) =>
+  const jobsTable = jobsDB.allJobs(false, [0, 200]).map((job) =>
     `<tr class="${job.status}"><td>${job.job.id}</td><td>${job.status}</td><td><a href="${job.dir}/log.txt">${job.dir}/log.txt</a></td><td>${job.job.from}</td><td>${job.job.till}</td></tr>`
   );
   return (`
+<!DOCTYPE html>
 <html>
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -37,6 +38,22 @@ export function indexPage(
     <tr><th>Job ID</th><th>Status</th><th>Log</th><th>From</th><th>Till</th></tr>
     ${jobsTable.join("\n")}
   </table>
+  <button id="loadall">Load All</button>
+  <script>
+  const button = document.getElementById("loadall");
+  button.addEventListener("click", async () => {
+    button.setAttribute("disabled", true);
+    const response = await fetch("jobs.json?from=201&till=-1");
+    const jobs = await response.json();
+    for (const jobStatus of jobs) {
+        const row = document.createElement("tr");
+        jobsTable.appendChild(row);
+        row.classList.add(jobStatus.status);
+        row.innerHTML = \`<td>\${jobStatus.job.id}</td><td>\${jobStatus.status}</td><td><a href="\${jobStatus.dir}/log.txt">\${jobStatus.dir}/log.txt</a></td><td>\${jobStatus.job.from}</td><td>\${jobStatus.job.till}</td>\`;
+    }
+    button.parentElement.removeChild(button);
+  });
+  </script>
 </body>
 </html>`);
 }
